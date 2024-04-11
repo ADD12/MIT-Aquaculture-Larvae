f94b7c4add909794df6272c77b831b6c5b766a32
unyimeu <134160334+unyimeu@users.noreply.github.com>
2024-04-11 15:00:54 -0400

    Update get_masks.py

--- get_masks.py
+++ get_masks.py
@@ -48,13 +48,14 @@ def load_image_paths(images_path,annotations):
         
     return image_paths
 
-def create_mask(image_info, annotations, output_folder):
+def create_mask(og_image_path,image_info, annotations, output_folder,im_output,cnt):
     # Create an empty mask as a numpy array
     mask_np = np.zeros((image_info['height'], image_info['width']), dtype=np.uint16)
 
     # Counter for the object number
-    class1 = 1
-    class2 = 2
+    class1 = 1 #class for not healthy
+    class2 = 2 #class for healthy
+    id = image_info['id']
 
     for ann in annotations:
         if ann['image_id'] == image_info['id']:
@@ -73,35 +74,47 @@ def create_mask(image_info, annotations, output_folder):
                     mask_np[rr, cc] = class1        
 
 
+    # Visualize the mask while making it to make sure it is correct
+    visualize_mask(mask_np)
+
+    # Save the numpy array as a TIFF using tifffile library
+    mask_name = "mask_{}.tif".format(cnt)
+    mask_path = os.path.join(output_folder, mask_name)
+    tifffile.imwrite(mask_path, mask_np)
+
+    #colored image
+    image_name = "image_{}.tif".format(cnt)
+    original = image_info['file_name']
+    path_im = os.path.join(og_image_path,original)
+    get_image = cv.imread(path_im, cv.IMREAD_COLOR)
+    im_path = os.path.join(im_output, image_name)
+    tifffile.imwrite(im_path, get_image)
+
+    print('shapes')
+    print(mask_np.shape)
+    print(get_image.shape)
+    print('--------------')
+
+    #print(f"Saved mask and image for {image_info['file_name']} to {mask_path}")
+
+
+def visualize_mask(mask_np):
     # Visualize the mask
     plt.imshow(mask_np, cmap='gray')  # You can change the colormap as needed
     plt.colorbar()
     plt.title('Mask Visualization')
     plt.show()
 
-    # Save the numpy array as a TIFF using tifffile library
-    mask_path = os.path.join(output_folder, image_info['file_name'].replace('.jpg', '_mask.tif'))
-    #mask_np_normalized = (mask_np * 255).astype(np.uint8)
-    mask_np_normalized = mask_np.astype(np.float32) / np.max(mask_np)
-    tifffile.imwrite(mask_path, mask_np_normalized)
-    #tifffile.imwrite(mask_path, mask_np)
 
-    print(f"Saved mask for {image_info['file_name']} to {mask_path}")
-
-
-def images_and_features(current,images,labels,output):
+def loop_images(og_image_path,labels,output_bi,output_im):
     cnt = 0
     images = labels['images']
     annotations = labels['annotations']
+
     for img in images:
 
-        #print('AT IMAGE------------', img)
-        #im = cv.imread(str(images[id]))
-        #im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-
-        create_mask(img,annotations,output)
-
-
+        create_mask(og_image_path,img,annotations,output_bi,output_im,cnt)
+        cnt+=1
 
 
 def main():
@@ -109,16 +122,23 @@ def main():
     api_key = "GTZOIW8tEAHwMJFcuMCM"
     workspace_name = "river-herring"
     project_name = "aquaculture-larvae"
-    version = 2
+    version = 5
+    print('loading from roboflow...')
     load_roboflow_data(api_key, workspace_name, project_name, version) # only needs to be run once
 
-
-    annotations_path = 'C:\\Users\\Unyim\\Downloads\\CSDownloads\\IAP_UROP_DATA\\Code\\Load_Data\\Aquaculture-Larvae-2\\train\\_annotations.coco.json'
+    #get annotation data
+    annotations_path = 'C:\\Users\\Unyim\\Downloads\\CSDownloads\\IAP_UROP_DATA\\Code\\Load_Data\\Aquaculture-Larvae-5\\train\\_annotations.coco.json' #path to annotations
     annotations_data = load_annotations(annotations_path)
-    images_path = 'Aquaculture-Larvae-2\\train'
+
+    #get image data
+    images_path = 'C:\\Users\\Unyim\\Downloads\\CSDownloads\\IAP_UROP_DATA\\Code\\Load_Data\\Aquaculture-Larvae-5\\train'  #path to images
     image_paths = load_image_paths(images_path,annotations_data)
 
-    mask_output_folder = 'binary_output' # Modify this as needed. Using val2 so my data is not overwritten
-    image_output_folder = '/content/drive/MyDrive/Code Projects/Data/train_im_output'  #
-    print(image_paths)
-    a1 = images_and_features('its a go',image_paths,annotations_data,mask_output_folder)
+    #output folders
+    mask_output_folder = 'C:\\Users\\Unyim\\Downloads\\CSDownloads\\IAP_UROP_DATA\\Code\\binary_output' # choose output folder for the masks
+    image_output_folder = 'C:\\Users\\Unyim\\Downloads\\CSDownloads\\IAP_UROP_DATA\\Code\\im_output'  # choose output folder for the images
+
+    print('looping through images')
+    loop_images(images_path,annotations_data,mask_output_folder,image_output_folder)
+
+main()
