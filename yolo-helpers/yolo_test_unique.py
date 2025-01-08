8a4172dc6f3159ca57837d797af244bbc8b33755
beckettld <121354080+beckettld@users.noreply.github.com>
2025-01-07 21:18:29 -0500

    Update yolo_test_unique.py

--- yolo-helpers/yolo_test_unique.py
+++ yolo-helpers/yolo_test_unique.py
@@ -44,6 +44,6 @@ def process_images(input_folder, conf_threshold=0.25):
     cv2.destroyAllWindows()
 
 # Example usage
-input_folder = '/Users/beckettdevoe/Desktop/Larvae Main/10x'  # Adjust path as necessary
+input_folder = '/Path/to/input/folder'  # Adjust path as necessary
 
 process_images(input_folder, conf_threshold=0.5)
